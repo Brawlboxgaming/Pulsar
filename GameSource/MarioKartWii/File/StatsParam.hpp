375bf6df876caf8b8d48e80348a884b5f7f6f0be
Brawlboxgaming <brawlboxgaming@gmail.com>
2024-02-25 17:26:23 +0000

    Renamed SetMsgId to SetMessage

--- GameSource/MarioKartWii/File/StatsParam.hpp
+++ GameSource/MarioKartWii/File/StatsParam.hpp
@@ -25,22 +25,22 @@ struct StatsParamFile {
         KartType type;
         u32 weightClass;
         float float_0xC;
-        float weight; //10
-        float bumpDeviationLevel;
+        float weight; //0x10
+        float bumpDeviationLevel; //0x14
         float baseSpeed; //0x18
-        float handlingSpeedMultiplier;
-        float tilt;
-        float standard_acceleration_as[4];
-        float standard_acceleration_ts[3];
-        float drift_acceleration_as[2];
-        float drift_acceleration_ts[1];
-        float manualHandling;
-        float autoHandling;
-        float handlingReactivity;
-        float manualDrift;
-        float automaticDrift;
-        float driftReactivity;
-        float targetAngle;
+        float handlingSpeedMultiplier; //0x1c
+        float tilt; //0x20
+        float standard_acceleration_as[4]; //0x24
+        float standard_acceleration_ts[3]; //0x34
+        float drift_acceleration_as[2]; //0x40
+        float drift_acceleration_ts[1]; //0x48
+        float manualHandling; //0x4c
+        float autoHandling; //0x50
+        float handlingReactivity; //0x54
+        float manualDrift; //0x58
+        float automaticDrift; //0x5c
+        float driftReactivity; //0x60
+        float targetAngle; //0x64
         float unknown_0x68;
         u32 mt;
         float speedFactors[32];
